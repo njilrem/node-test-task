9d1c89b2c3ed09d75342df03cfa3f9a17cebc2f4
njilrem <antonkucher00@gmail.com>
2020-07-09 10:54:13 +0300

    added babel, moved to src, configured package.json

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import usersRouter from './routes/users.js'
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/users', usersRouter);
+
+export default app
